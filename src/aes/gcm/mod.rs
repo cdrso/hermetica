b6fa021899e622b5f75dc4389c70a951b956f4b9
Alejandro Cadarso <acadarsotp@gmail.com>
2024-07-11 00:20:34 +0200

    ª

--- src/aes/gcm/mod.rs
+++ src/aes/gcm/mod.rs
@@ -11,15 +11,13 @@ use bytemuck::{bytes_of, bytes_of_mut};
 use indicatif::{ProgressBar, ProgressStyle};
 use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
 
-// https://software.intel.com/sites/default/files/managed/72/cc/clmul-wp-rev-2.02-2014-04-20.pdf
-
 pub enum GcmMode {
     Encryption,
     Decryption
 }
 
 pub struct GcmInstance {
-    key_schedule: [u32; 44], //gen from key, no need for key_schedule_decrypt | owned
+    key_schedule: [u32; 44],
 }
 
 #[derive(Debug)]
@@ -49,7 +47,7 @@ struct GcmEncrypt;
 struct GcmDecrypt;
 
 trait GcmOperation: Sync {
-    fn process_block(&self, block: &[u8], counter: u128, key_schedule: &[u32; 44]) -> Vec<u8>;
+    fn process_block(&self, block: &[u8], counter: u128, key_schedule: &[u32; 44]) -> [u8; 16];
     fn update_tag(&self, block: &[u8], tag: &mut u128, h: u128);
 }
 
@@ -62,16 +60,15 @@ pub(crate) fn gf_mult(operand_a: u128, operand_b: u128) -> u128 {
             bytes_of_mut(&mut product).as_mut_ptr(),
         );
     }
-
     product
 }
 
 impl GcmOperation for GcmEncrypt {
-    fn process_block(&self, block: &[u8], counter: u128, key_schedule: &[u32; 44]) -> Vec<u8> {
+    fn process_block(&self, block: &[u8], counter: u128, key_schedule: &[u32; 44]) -> [u8; 16] {
         let encrypted_counter = aes::encrypt_block(*key_schedule, counter);
-        let mut result = Vec::with_capacity(16);
+        let mut result = [0u8; 16];
         for (i, &byte) in block.iter().enumerate() {
-            result.push(byte ^ ((encrypted_counter >> (8 * (15 - i))) as u8));
+            result[i] = byte ^ ((encrypted_counter >> (8 * (15 - i))) as u8);
         }
         result
     }
@@ -82,11 +79,11 @@ impl GcmOperation for GcmEncrypt {
 }
 
 impl GcmOperation for GcmDecrypt {
-    fn process_block(&self, block: &[u8], counter: u128, key_schedule: &[u32; 44]) -> Vec<u8> {
+    fn process_block(&self, block: &[u8], counter: u128, key_schedule: &[u32; 44]) -> [u8; 16] {
         let encrypted_counter = aes::encrypt_block(*key_schedule, counter);
-        let mut result = Vec::with_capacity(16);
+        let mut result = [0u8; 16];
         for (i, &byte) in block.iter().enumerate() {
-            result.push(byte ^ ((encrypted_counter >> (8 * (15 - i))) as u8));
+            result[i] = byte ^ ((encrypted_counter >> (8 * (15 - i))) as u8);
         }
         result
     }
@@ -97,6 +94,7 @@ impl GcmOperation for GcmDecrypt {
 }
 
 const BUFFER_SIZE: usize = 1 << 20;
+
 impl GcmInstance {
     pub fn new(key: u128) -> Self {
         let key_schedule = aes::gen_encryption_key_schedule(key);
@@ -111,7 +109,6 @@ impl GcmInstance {
             GcmMode::Encryption => {
                 let mut output_path = input_path.clone();
                 output_path.as_mut_os_string().push(".hmtc");
-                //TODO
                 let iv: [u8; 12] = rand::thread_rng().gen();
                 (output_path, iv)
             },
@@ -132,64 +129,71 @@ impl GcmInstance {
 
     fn compute<T: GcmOperation + ?Sized>(
         &self,
-        input: &mut BufReader<fs::File>,
-        output: &mut BufWriter<fs::File>,
+        input: &mut BufReader<File>,
+        output: &mut BufWriter<File>,
         iv: &[u8; 12],
         op: &T,
     ) -> GcmResult<u128> {
         let h = aes::encrypt_block(self.key_schedule, 0u128);
         let mut tag = gf_mult(h, u128::from_ne_bytes([&iv[..], &[0u8; 4]].concat().try_into().unwrap()));
 
-        // esto ???
         let length = input.get_ref().metadata()?.len() as usize;
         let bufread_cnt = (length + BUFFER_SIZE - 1) / BUFFER_SIZE;
 
         let pb = ProgressBar::new(bufread_cnt as u64);
         pb.set_style(ProgressStyle::with_template(
-                "{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})",
+            "{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})",
         ).unwrap());
 
         let thread_num = thread::available_parallelism()?.get();
         let mut ctr: u32 = 1;
 
         for _ in 0..bufread_cnt {
-            let mut buffer = vec![0u8; BUFFER_SIZE];
+            let mut buffer = [0u8; BUFFER_SIZE];
             let bytes_read = input.read(&mut buffer)?;
-            buffer.truncate(bytes_read);
-            let blocks: Vec<&[u8]> = buffer.chunks(16).collect();
-            let blocks_per_thread = (blocks.len() + thread_num - 1) / thread_num;
+            let blocks_count = (bytes_read + 15) / 16;
+            let blocks_per_thread = (blocks_count + thread_num - 1) / thread_num;
 
             thread::scope(|s| {
                 let mut handles = vec![];
-                for (thread_index, chunk) in blocks.chunks(blocks_per_thread).enumerate() {
+                for thread_index in 0..thread_num {
+                    let start = thread_index * blocks_per_thread * 16;
+                    let end = (start + blocks_per_thread * 16).min(bytes_read);
+                    if start >= end {
+                        break;
+                    }
                     let handle = s.spawn(move || {
                         let mut local_tag = 0u128;
-                        let mut processed = Vec::new();
-                        for (i, block) in chunk.iter().enumerate() {
+                        let mut processed = [0u8; BUFFER_SIZE];
+                        let mut processed_len = 0;
+                        for (i, chunk) in buffer[start..end].chunks(16).enumerate() {
                             let counter = u128::from_ne_bytes([&iv[..], &(ctr + (thread_index * blocks_per_thread + i) as u32).to_be_bytes()].concat().try_into().unwrap());
-                            let processed_block = op.process_block(block, counter, &self.key_schedule);
-                            op.update_tag(&processed_block, &mut local_tag, h);
-                            processed.extend_from_slice(&processed_block);
-                            dbg!(&processed);
+                            let mut block = [0u8; 16];
+                            block[..chunk.len()].copy_from_slice(chunk);
+                            let processed_block = op.process_block(&block, counter, &self.key_schedule);
+                            op.update_tag(&processed_block[..chunk.len()], &mut local_tag, h);
+                            processed[processed_len..processed_len + chunk.len()].copy_from_slice(&processed_block[..chunk.len()]);
+                            processed_len += chunk.len();
                         }
-                        (local_tag, processed)
+                        (local_tag, processed, processed_len)
                     });
                     handles.push(handle);
                 }
 
                 for handle in handles {
-                    let (local_tag, processed) = handle.join().unwrap();
+                    let (local_tag, processed, processed_len) = handle.join().unwrap();
                     tag ^= local_tag;
-                    output.write_all(&processed)?;
+                    output.write_all(&processed[..processed_len])?;
                 }
                 Ok::<_, std::io::Error>(())
             })?;
 
-            ctr += blocks.len() as u32;
+            ctr += blocks_count as u32;
             pb.inc(1);
         }
 
         pb.finish_with_message("done");
+        tag ^= length as u128;
         Ok(tag)
     }
 
@@ -213,17 +217,18 @@ impl GcmInstance {
         Ok(())
     }
 
-    pub fn process(&self, input_path: PathBuf, mode: GcmMode) -> GcmResult<(u128, PathBuf)> {
-        let (mut input, mut output, iv, output_path) = self.init(&input_path, &mode)?;
+    pub fn encrypt(&self, plain_text: PathBuf) -> GcmResult<(u128, PathBuf)> {
+        let (mut input, mut output, iv, output_path) = self.init(&plain_text, &GcmMode::Encryption)?;
+        output.write_all(&iv)?;
+        let tag = self.compute(&mut input, &mut output, &iv, &GcmEncrypt)?;
+        self.finalize(&mut output, tag, &GcmMode::Encryption, &output_path)?;
+        Ok((tag, output_path))
+    }
 
-        let op: &dyn GcmOperation = match mode {
-            GcmMode::Encryption => &GcmEncrypt,
-            GcmMode::Decryption => &GcmDecrypt,
-        };
-
-        let tag = self.compute(&mut input, &mut output, &iv, op)?;
-        self.finalize(&mut output, tag, &mode, &output_path)?;
-
+    pub fn decrypt(&self, cypher_text: PathBuf) -> GcmResult<(u128, PathBuf)> {
+        let (mut input, mut output, iv, output_path) = self.init(&cypher_text, &GcmMode::Decryption)?;
+        let tag = self.compute(&mut input, &mut output, &iv, &GcmDecrypt)?;
+        self.finalize(&mut output, tag, &GcmMode::Decryption, &output_path)?;
         Ok((tag, output_path))
     }
 }
